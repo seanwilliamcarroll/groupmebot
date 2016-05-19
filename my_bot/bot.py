117876228293fbaf9daba127d6a473ad1bb8bb1a
Sean Carroll <swc63@cornell.edu>
2016-05-19 18:57:52 -0400

    Rearranging, new function

--- my_bot/bot.py
+++ my_bot/bot.py
@@ -3,32 +3,24 @@
 # Attempt at building a groupme bot
 
 import requests
-from bot_server import BotServer, BotServerRequestHandler
 
-try:
-  import config
-  group = config.group
-  bot_id = config.bot_id
-except:
-  import gen_config
-  group = gen_config.group
-  bot_id = gen_config.bot_id
 
 def send_message(message, bot):
+  try:
+    import config
+    group = config.group
+    bot_id = config.bot_id
+  except:
+    import gen_config
+    group = gen_config.group
+    bot_id = gen_config.bot_id
   # Helper function to send a message
   payload = {'bot_id':bot, 'text':message}
-  print payload
   r = requests.post("https://api.groupme.com/v3/bots/post", data=payload)
-  print r.status_code
+  # print r.status_code
+  return r.text
 
 def parrot():
   # Request last message sent in specific group
   # Parse message, send back same message
-  
   pass
-
-def main():
-  send_message('System test', bot_id)
-  parrot()
-
-main()
--- my_bot/views.py
+++ my_bot/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http      import HttpResponse
+import bot
 
 
 def index(request):
   return HttpResponse("Hello, world. You're at my_bot's index. Git update")
+
+def message(request):
+  return HttpResponse(bot.send_message())
