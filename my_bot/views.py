35c21dabde24702a91ffe79ebb9cae582bad34b4
Sean Carroll <swc63@cornell.edu>
2016-05-20 11:05:20 -0400

    Begin POST work

--- my_bot/views.py
+++ my_bot/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from django.http      import HttpResponse
+from django.http      import HttpResponse, HttpResponseNotFound
 import bot
 import datetime
 
 
 def index(request):
-  return HttpResponse("Hello, world. You're at my_bot's index. Git update")
+  if request.POST:
+    return HttpResponse(request.body)
+  else if request.GET:
+    return HttpResponse(request.body)
+    #return HttpResponse("Hello, world. You're at my_bot's index. Git update")
+  else:
+    return HttpResponseNotFound("Bad request")
 
 def message(request):
   message = datetime.datetime.now()
